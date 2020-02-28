c70f2ab22d3241aa800e00232bf763082b73670a
Muna Chimoriya <muna.chimoriya.2022@student.lhschools.org>
2020-02-28 12:14:13 -0500

    intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,33 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|        United Dairy Farmers                |");
+        System.out.println("|            Store #679                      |");
+        System.out.println("|    7905 E Broad ST, Blacklick,OH 43004     |");
+        System.out.println("|  ORIGINAL                                  |");
+        System.out.println("|Receipt                  2145450            |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|               Take Away                    |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|   Qty      Name             Price    Total |");
+        System.out.println("|   One   Prepaid Regular   $2.43/G   $10.00 |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|Total    $10.00                             |");
+        System.out.println("|Debit                                       |");
+        System.out.println("|Acc       XXXXXXXXXXXX0277       SWIPED     |");
+        System.out.println("|Ref            2626                         |");
+        System.out.println("|Approval       222921                       |");
+        System.out.println("|Batch 2  SEQ        2626                    |");
+        System.out.println("|Approved on       1/28/19   13:33:15        |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|01/12/2020 1:33:26 PM  POS:2   Cashier:108  |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|  We look forward to seeing you again!      |");
+        System.out.println("|--------------------------------------------|");
+
+
+    }
+}
